73f1064ef06a203b8a8a376f9fdabc82be114e41
Stefan Ziegler <edi.gonzales@gmail.com>
2018-05-30 11:21:33 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gdi-dev-env
+
+Rollen erstellen:
+```
+sudo -u postgres psql -d postgres -f create_roles.sql
+```
+
+Pub-DB-Dump einspielen:
+```
+pg_restore --role=postgres --exit-on-error -C -d postgres pub.dmp
+```
---
+++ create_roles.sql
@@ -0,0 +1,9 @@
+CREATE ROLE ddluser SUPERUSER LOGIN PASSWORD 'ddluser';
+CREATE ROLE dmluser LOGIN PASSWORD 'dmluser';
+
+CREATE ROLE sogis_admin LOGIN PASSWORD 'sogis_admin';
+CREATE ROLE admin LOGIN PASSWORD 'admin';
+CREATE ROLE ogc_server LOGIN PASSWORD 'ogc_server';
+CREATE ROLE sogis_service LOGIN PASSWORD 'sogis_service';
+CREATE ROLE gretl LOGIN PASSWORD 'gretl';
+CREATE ROLE barpastu LOGIN PASSWORD 'barpastu';
